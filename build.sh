63345dd8b2052d064a77c1e18cebf117e5a18d47
Periaraperi <periaraperi@protonmail.com>
2023-05-23 16:14:19 +0400

    modifed structure, added sub projects, engine is built as shared lib

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 build_type=$1
-if [ "$build_type" = --debug ]; then
+target=$2
+if [ "$build_type" = "--debug" ]; then
     echo Compiling Debug Build
-    cmake --build build/debug/ --config Debug
+    echo Compiling engine library
+    cmake --build build/debug/ --config Debug --target engine
+    echo Compiling target $target
+    cmake --build build/debug/ --config Debug --target $target
 elif [ "$build_type" = "--release" ]; then
     echo Compiling Release Build
-    cmake --build build/release/ --config Release
+    echo Compiling engine library
+    cmake --build build/release/ --config Release --target engine
+    echo Compiling target $target
+    cmake --build build/release/ --config Release --target $target
 else
     echo must specify build type
 fi
